558ad44f331651fb7047c5edf411f2c8651445a5
Satit Riapit <siteslave@msn.com>
2019-08-16 09:59:48 +0700

    update leave settings

--- src/models/leave-setting.ts
+++ src/models/leave-setting.ts
@@ -27,4 +27,75 @@ export class LeaveSettingModel {
       .del();
   }
 
+  // initial leave 
+  // 1. get all employees
+  getAllEmployeesWithLeaveType(db: knex) {
+    return db('empployees')
+      .where('is_enabled', 'Y');
+  }
+
+  getLeaveDaysSettings(db: knex, periodId: number) {
+    return db('leave_days_settings')
+      .where('period_id', periodId);
+  }
+
+  getAllLeaveSummary(db: knex, periodId: number) {
+    let sql = `
+      select e.employee_id, e.first_name,e.last_name, e.employee_type_id,
+      et.employee_type_name, 
+      lt.leave_type_name, lds.leave_type_id,
+      lds.leave_days, lds.max_leave_days, lds.is_collect,
+      (
+        select ifnull(sum(l.leave_days), 0) 
+        from leaves as l 
+        where l.employee_id=e.employee_id 
+        and l.leave_type_id=lds.leave_type_id
+        and l.period_id=?
+        ) as total_leave_days,
+      (
+        select ifnull(sum(ldd.leave_days_num), 0) 
+        from leave_days as ldd 
+        where ldd.employee_id=e.employee_id 
+        and ldd.leave_type_id=lds.leave_type_id
+        and ldd.period_id=?
+      ) as collected_leave_days
+      from employees as e 
+      inner join employee_types as et on et.employee_type_id=e.employee_type_id
+      inner join leave_days_settings as lds on lds.employee_type_id=e.employee_type_id 
+      and lds.employee_type_id=e.employee_type_id and lds.period_id=?
+      inner join leave_types as lt on lt.leave_type_id=lds.leave_type_id
+
+      order by e.employee_id
+    `;
+
+    return db.raw(sql, [periodId, periodId, periodId]);
+  }
+
+  removeLeaveDaysForNextPeriod(db: knex, periodId: number) {
+    return db('leave_days')
+      .where('period_id', periodId)
+      .del();
+  }
+
+  saveLeaveDaysNewPeriod(db: knex, data: any[]) {
+    return db('leave_days')
+      .insert(data);
+  }
+
+  getCurrentLeaveDaysSetting(db: knex, periodId: number) {
+    return db('leave_days_settings')
+      .where('period_id', periodId);
+  }
+
+  removeOldLeaveDaysSetting(db: knex, periodId: number) {
+    return db('leave_days_settings')
+      .where('period_id', periodId)
+      .del();
+  }
+
+  saveNewLeaveDaysSetting(db: knex, data: any[]) {
+    return db('leave_days_settings')
+      .insert(data);
+  }
+
 }
--- src/routes/leave-settings.ts
+++ src/routes/leave-settings.ts
@@ -1,17 +1,9 @@
 /// <reference path="../../typings.d.ts" />
 
 import { Request, Response, Router } from 'express';
-import * as ejs from 'ejs';
-import * as fs from 'fs';
-import * as path from 'path';
-import * as fse from 'fs-extra';
-import * as pdf from 'html-pdf';
-import * as rimraf from 'rimraf';
-import * as moment from 'moment';
 
-import { LeaveModel } from "../models/leave";
-import { LeaveTypeModel } from '../models/leave_type';
-import { EmployeeModel } from '../models/employee';
+import * as _ from 'lodash';
+
 import { LeaveSettingModel } from '../models/leave-setting';
 
 const leaveSettingModel = new LeaveSettingModel();
@@ -26,6 +18,39 @@ router.post('/settings', async (req: Request, res: Response) => {
     const rs: any = await leaveSettingModel.getSetting(req.db, periodId, employeeTypeId);
 
     res.send({ ok: true, rows: rs[0] });
+  } catch (e) {
+    console.log(e);
+    res.send({ ok: false, code: 500, error: 'เกิดข้อผิดพลาด' });
+  }
+});
+
+router.post('/copy-leave-settings', async (req: Request, res: Response) => {
+  try {
+    const oldPeriodId = req.body.oldPeriodId;
+    const nextPeriodId = req.body.nextPeriodId;
+    const db = req.db;
+
+    const rs: any = await leaveSettingModel.getCurrentLeaveDaysSetting(req.db, oldPeriodId);
+
+    let data = [];
+
+    rs.forEach(v => {
+      let obj: any = {};
+      obj.employee_type_id = v.employee_type_id;
+      obj.leave_type_id = v.leave_type_id;
+      obj.leave_days = v.leave_days;
+      obj.period_id = nextPeriodId;
+      obj.is_collect = v.is_collect;
+
+      data.push(obj);
+    });
+
+    // remove old setting
+    await leaveSettingModel.removeOldLeaveDaysSetting(db, nextPeriodId);
+    // save new setting
+    await leaveSettingModel.saveNewLeaveDaysSetting(db, data);
+
+    res.send({ ok: true });
   } catch (e) {
     console.log(e);
     res.send({ ok: false, code: 500, error: 'เกิดข้อผิดพลาด' });
@@ -49,6 +74,68 @@ router.post('/save-settings', async (req: Request, res: Response) => {
     } else {
       res.send({ ok: false, error: 'ไม่พบข้อมูลที่ต้องการบันทึก' });
     }
+
+  } catch (e) {
+    console.log(e);
+    res.send({ ok: false, code: 500, error: 'เกิดข้อผิดพลาด' });
+  }
+});
+
+router.post('/initial-leave', async (req: Request, res: Response) => {
+  const db = req.db;
+  const oldPeriodId = req.body.oldPeriodId;
+  const nextPeriodId = req.body.nextPeriodId;
+
+  try {
+    // TODO: Initial leave days
+    // get summary for this period
+    const rsSummary = await leaveSettingModel.getAllLeaveSummary(db, oldPeriodId);
+
+    // console.log(rsSummary[0]);
+    // calculate new leave days for new period
+    let leaveDaysData = [];
+    const summary = rsSummary[0];
+
+    summary.forEach(v => {
+      let obj: any = {};
+
+      // leave_days = สิทธิ์ที่ได้รับกี่วัน, 0 = ไม่จำกัด
+      // max_leave_days สะสมได้ไม่เกินกี่วัน
+
+      obj.employee_id = +v.employee_id;
+      obj.period_id = +nextPeriodId;
+      obj.leave_type_id = +v.leave_type_id; // ประเภทการลา
+
+      const leaveDays = +v.leave_days
+
+      obj.leave_days_num = leaveDays; // โค้วต้าในการลาทั้งหมดของปีงบประมาณ ค่าปกติได้จาก
+
+      if (v.is_collect === 'Y') { // สามารถสะสมวันลาได้ (สำหรับประเภทนั้นเท่านั้น)
+        // วันลาคงเหลือของปีงบปัจจุบัน
+        let remainDays = +v.collected_leave_days - +v.total_leave_days;
+        // วันลาที่มีสิทธิ์ลาได้ทั้งหมดในปีงบประมาณถัดไป
+        let totalLeaveDays = remainDays + leaveDays;
+        // โควต้าสูงสุดในการสะสม ในกรณีที่สะสมได้
+        const maxCollectedLeaveDays = +v.max_leave_days || 0;
+
+        if (totalLeaveDays > maxCollectedLeaveDays) { // วันลาคงเหลือ + สะสม เกินกว่าโควต้าที่ได้รับ
+          obj.leave_days_num = maxCollectedLeaveDays; // โควต้าสูงสุดในการสะสม 
+        } else {
+          obj.leave_days_num = totalLeaveDays; // ใช้วันลารวมสะสมทั้งหมด
+        }
+      }
+
+      leaveDaysData.push(obj);
+
+    });
+    // save new leave days for new period
+    console.log(leaveDaysData);
+
+    // remove old data
+    await leaveSettingModel.removeLeaveDaysForNextPeriod(db, nextPeriodId);
+    await leaveSettingModel.saveLeaveDaysNewPeriod(db, leaveDaysData);
+
+    res.send({ ok: true });
 
   } catch (e) {
     console.log(e);
